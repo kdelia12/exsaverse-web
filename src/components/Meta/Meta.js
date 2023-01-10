10a3e4aea15c1087771347cb7d39fa547167920f
kdelia12 <wild12748@gmail.com>
2023-01-11 02:03:54 +0700

    adding social banner 2

--- src/components/Meta/Meta.js
+++ src/components/Meta/Meta.js
@@ -16,7 +16,7 @@ export const Meta = ({ title, description, prefix = name, ogImage = defaultOgIma
 
       <meta property="og:image" content={ogImage} />
       <meta property="og:image:alt" content="Banner for the site" />
-      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:type" content="image/jpeg" />
       <meta property="og:image:width" content="1280" />
       <meta property="og:image:height" content="675" />
 
